4eeea5f4340f7fae410e81b9ca747db6b5bc2236
Alliiiii <47424634+Alliiiii@users.noreply.github.com>
2020-06-04 17:43:30 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      debugShowCheckedModeBanner:false,
+      home: VoiceHome(),
+    );
+  }
+}
+
+class VoiceHome extends StatefulWidget {
+  @override
+  _VoiceHomeState createState() => _VoiceHomeState();
+}
+
+class _VoiceHomeState extends State<VoiceHome> {
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+
+  String resultText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechRecognizer();
+  }
+
+  void initSpeechRecognizer() {
+    _speechRecognition = SpeechRecognition();
+
+    _speechRecognition.setAvailabilityHandler(
+          (bool result) => setState(() => _isAvailable = result),
+    );
+
+    _speechRecognition.setRecognitionStartedHandler(
+          () => setState(() => _isListening = true),
+    );
+
+    _speechRecognition.setRecognitionResultHandler(
+          (String speech) => setState(() => resultText = speech),
+    );
+
+    _speechRecognition.setRecognitionCompleteHandler(
+          () => setState(() => _isListening = false),
+    );
+
+    _speechRecognition.activate().then(
+          (result) => setState(() => _isAvailable = result),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title:Text("Speech Reecoginition App",style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+        backgroundColor: Colors.purple,
+
+      ),
+
+      body: Container(
+
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('Artifical Intelligence Task 2',style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black,fontSize: 25.0),),
+            Text('Group Members:',style: TextStyle( color: Colors.black,fontSize: 20.0),),
+            Text('Muhammad Ali               9363',style: TextStyle( color: Colors.black,fontSize: 20.0),),
+            Text('Danial Younus                8161',style: TextStyle( color: Colors.black,fontSize: 20.0),),
+            Text('M Kashan                       8876',style: TextStyle( color: Colors.black,fontSize: 20.0),),
+            Text('Waleed Abbas                8799',style: TextStyle( color: Colors.black,fontSize: 20.0),),
+
+
+
+
+
+            Row(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+
+                FloatingActionButton(
+                  child: Icon(Icons.cancel),
+                  mini: true,
+                  backgroundColor: Colors.deepOrange,
+                  onPressed: () {
+                    if (_isListening)
+                      _speechRecognition.cancel().then(
+                            (result) => setState(() {
+                          _isListening = result;
+                          resultText = "";
+                        }),
+                      );
+                  },
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.mic),
+                  onPressed: () {
+                    if (_isAvailable && !_isListening)
+                      _speechRecognition
+                          .listen(locale: "en_US")
+                          .then((result) => print('$result'));
+                  },
+                  backgroundColor: Colors.pink,
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.stop),
+                  mini: true,
+                  backgroundColor: Colors.deepPurple,
+                  onPressed: () {
+                    if (_isListening)
+                      _speechRecognition.stop().then(
+                            (result) => setState(() => _isListening = result),
+                      );
+                  },
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(
+                color: Colors.cyanAccent[100],
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 12.0,
+              ),
+              child: Text(
+                resultText,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
